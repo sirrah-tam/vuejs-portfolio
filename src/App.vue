af223210cc0e260fd0f911bba7c8523098e441ca
Mat <matharris@sirrahtam.com>
2017-11-02 19:26:16 -0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="app">
+	<page-header></page-header>
+	<img src="./assets/logo.png">
+	<router-view/>
+</div>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+	name: 'app',
+	components: {
+		'page-header': Header
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+		<nav class="navbar navbar-inverse navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<a href="index.html" class="navbar-brand">
+						{{ siteName }}
+					</a>
+					<div class="collapse navbar-collapse pull-right" id="main-nav">
+						<ul class="nav navbar-nav">
+							<li><a href="#about">About</a></li>
+							<li><a href="#projects">Projects</a></li>
+							<li><a href="#profilesTTH">Profiles</a></li>
+						</ul>
+					</div>
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+			</div>
+		</nav>
+	</header>
+</template>
+
+<script>
+    export default {
+        name: 'page-header',
+        data () {
+            return {
+                siteName: 'Sirrah Tam | Mat Harris'
+            }
+        }
+    }
+</script>
