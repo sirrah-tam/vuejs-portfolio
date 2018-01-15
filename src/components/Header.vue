34b28186ccbee314ba43c29527788ad7ccfe0a0f
Mat Harris <hurris88@gmail.com>
2018-01-14 16:50:42 -0800

    ^ add responsive viewport meta tag, update menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,10 @@
 		<nav class="navbar navbar-dark bg-dark">
 			<div class="container">
                 <a href="/" class="navbar-brand">{{ info.siteName }}</a>
-                <main-nav :links="links"></main-nav>
+                <main-nav :links="links" id="main-nav" class="collapse"></main-nav>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 			</div>
 		</nav>
 	</header>
