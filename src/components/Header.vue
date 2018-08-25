b4d79b1986ead1d8fb487daf4e9d23c6db46e5dd
Mat Harris <hurris88@gmail.com>
2018-08-25 10:22:36 -0700

    Update main nav for better user experience

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,30 +1,31 @@
 <template>
-    <header class="sticky-top">
-		<nav class="navbar navbar-dark bg-dark">
-			<div class="container">
-                <a href="/" class="navbar-brand">{{ info.siteName }}</a>
-                <main-nav :links="links" id="main-nav" class="collapse"></main-nav>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-			</div>
-		</nav>
-	</header>
+  <header class="sticky-top">
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a href="/" class="navbar-brand">{{ info.siteName }}</a>
+        <main-nav :links="links" class="main-nav d-none d-sm-block"></main-nav>
+        <main-nav :links="links" id="main-nav-mobile" class="collapse d-sm-none"></main-nav>
+        <button class="navbar-toggler d-sm-none" type="button" data-toggle="collapse" data-target="#main-nav-mobile" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
-    import Nav from './Nav.vue'
+import Nav from './Nav.vue'
 
-    export default {
-        name: 'page-header',
+export default {
+  name: 'page-header',
 
-        props: {
-            links: Array,
-            info: Object
-        },
+  props: {
+    links: Array,
+    info: Object
+  },
 
-        components: {
-            'main-nav': Nav
-        }
-    }
+  components: {
+    'main-nav': Nav
+  }
+}
 </script>
